6cba8749a728342b7bcbe3be1583d96aa5d93c1d
taotao2010 <cih002@gmail.com>
2017-09-09 20:24:41 +0800

    UP

--- video_html5.user.js
+++ video_html5.user.js
@@ -1,25 +1,29 @@
 // ==UserScript==
 // @name             视频站启用html5播放器
 // @description      拥抱html5，告别Flash。支持站点：优.土、QQ、新浪、微博、搜狐、乐视、央视、风行等。并添加播放快捷键：快进、快退、暂停/播放、音量调节
-// @version          0.5.1
+// @version          0.5.5
 // @homepage         http://bbs.kafan.cn/thread-2093014-1-1.html
+// @include          *://pan.baidu.com/play/*
 // @include          https://*.qq.com/*
 // @exclude          *://live.qq.com/*
 // @exclude          *://user.qzone.qq.com/*
-// @exclude          *://lpl.qq.com/es/lpl/*
-// @exclude          *://qt.qq.com/zhibo/index.html*
+// @exclude          *://lpl.qq.com/*
+// @exclude          *://qt.qq.com/zhibo/*
 // @include          http://v.youku.com/v_show/id_*
 // @include          https://v.youku.com/v_show/id_*
 // @include          *://*.bilibili.com/*
 // include          http://*.cctv.com/*
 // @exclude          http://tv.cctv.com/live/*
 // include          http://*.cntv.cn/*
-// @include          *://video.sina.*.html*
+// @include          *://video.sina.*
 // @include          *://weibo.com/*
+// @include          *://www.weibo.com/*
 // @include          http://*.le.com/*.html*
 // @include          *://*.lesports.com/*.html*
-// @include          *://www.fun.tv/vplay/v-*
-// @include          *://www.fun.tv/vplay/g-*
+// @include          *://tv.sohu.com/*.shtml*
+// @include          *://*.tv.sohu.com/*.shtml*
+// @include          *://film.sohu.com/album/*
+// @include          *://www.fun.tv/vplay/*
 // @include          *://m.fun.tv/*
 // @include          https://www.panda.tv/*
 // @exclude          https://www.panda.tv/
@@ -38,12 +42,23 @@ String.prototype.r1 = function(r) {
 	return r.test(this) && RegExp.$1;
 };
 
-let siteFn, v, totalTime,
+let v, totalTime,
 	oldCanplay = null,
-	path = location.pathname;
-const stepLen = 5, //快进快退5秒
-skipLen = 27, //shift + 快进快退
-u = location.hostname,
+	playerInfo = {},
+	path = location.pathname,
+isFullScreen = () => {
+	return !!(document.fullscreen || document.webkitIsFullScreen || document.mozFullScreen ||
+        document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement);
+},
+//惰性函数
+requestFullScreen = v => {
+	const fn = v.requestFullscreen || v.webkitRequestFullScreen || v.mozRequestFullScreen || v.msRequestFullScreen;
+	if (fn) requestFullScreen = v => fn.call(v);
+	else requestFullScreen = () => {};
+	requestFullScreen(v);
+};
+
+const u = location.hostname,
 mDomain = u.startsWith('video.sina.') ? 'sina' : u.split('.').reverse()[1],//主域名
 ua_samsung = 'Mozilla/5.0 (Linux; U; Android 4.0.4; GT-I9300 Build/IMM76D) AppleWebKit/534.30 Version/4.0 Mobile Safari/534.30',
 q = css => document.querySelector(css),
@@ -63,16 +78,22 @@ getAllDuration = css => {
 	}
 	return n || 2e4;
 },
-onCanplay = function (e) {
-	//v.removeEventListener('oncanplay', onCanplay);
+doClick = css => {
+	if (!css) return !1;
+	const x = q(css);
+	x && x.click();
+	return !!x;
+},
+onCanplay = function(e) {
 	//v.oncanplay = null;//注释掉，应对列表点播而不刷新页面
 	console.log('脚本[启用html5播放器]，事件oncanplay');
+	if (playerInfo.onMetadata) {
+		playerInfo.onMetadata();
+		delete playerInfo.onMetadata;
+	}
 	oldCanplay && oldCanplay(e);
 	const r = path !== location.pathname;//点播了另一个视频
 	if (totalTime && !r) return;//分段视频返回
-	if (!totalTime) document.addEventListener('keydown', hotKey, !1);
-	totalTime = 0;
-	siteFn && siteFn();
 	totalTime = totalTime || Math.round(v.duration);
 	//跳过片头
 	if (totalTime > 666 && 'youku' !== mDomain) setTimeout( () => {
@@ -80,30 +101,76 @@ onCanplay = function (e) {
 	}, 9);
 	if (r) path = location.pathname;
 },
-hotKey = function (e) {
-	//console.log('hotKey', v.seeking, v.seekable);
-	// 可播放
-	//if (!v.seekable || v.readyState !== 4) return;
+hotKey = function(e) {
+	if (e.ctrlKey || e.altKey) return;
+	if (/INPUT|TEXTAREA/.test(e.target.nodeName)) return;
+	let n;
 	switch (e.keyCode) {
 	case 32: //space
+		if (e.shiftKey || playerInfo.disableSpace) return;
 		v.paused ? v.play() : v.pause();
 		e.preventDefault();
 		//e.stopPropagation();
 		break;
 	case 37: //left
-		v.currentTime -= e.shiftKey ? skipLen : stepLen;
+		n = e.shiftKey ? 27 : 5; //快退5秒,shift加速
+		v.currentTime -= n;
 		break;
 	case 39: //right
-		v.currentTime += e.shiftKey ? skipLen : stepLen;
+		n = e.shiftKey ? 27 : 5; //快进5秒,shift加速
+		v.currentTime += n;
 		break;
+	case 78: // N 下一首
+		if (e.shiftKey) return;
+		doClick(playerInfo.nextCSS);
+		break;
+	//case 80: // P 上一首
 	case 38: //加音量
-		//v.volume = v.volume.toFixed(2) + 0.01;
-		v.volume += 0.1;
+		n = v.volume + 0.1;
+		if (e.shiftKey || n > 1) return;
+		v.volume = n.toFixed(2);
 		e.preventDefault();
 		break;
 	case 40: //降音量
-		v.volume -= 0.1;
+		n = v.volume - 0.1;
+		if (e.shiftKey || n < 0) return;
+		v.volume = n.toFixed(2);
 		e.preventDefault();
+		break;
+	case 13: //全屏
+		if (e.shiftKey) doClick(playerInfo.webfullCSS);
+		else if (!isFullScreen()) doClick(playerInfo.fullCSS) || requestFullScreen(v);
+		break;
+	case 88: //按键X：减速播放 -0.1
+		if (e.shiftKey) return;
+		n = v.playbackRate;
+		if (n > 0.1) {
+			n -= 0.1;
+			v.playbackRate = n.toFixed(1);
+		}
+		break;
+	case 67: //按键C：加速播放 +0.1
+		if (e.shiftKey) return;
+		n = v.playbackRate;
+		if (n < 16) {
+			n += 0.1;
+			v.playbackRate = n.toFixed(1);
+		}
+		break;
+	case 90: //按键Z：正常速度播放
+		if (e.shiftKey) return;
+		v.playbackRate = 1;
+		break;
+	case 70: //按键F：下一帧
+		if (e.shiftKey) return;
+		if (!v.paused) v.pause();
+		v.currentTime += Number(1 / 30);
+		break;
+	case 68: //按键D：上一帧
+		if (e.shiftKey) return;
+		if (!v.paused) v.pause();
+		v.currentTime -= Number(1 / 30);
+		break;
 	}
 },
 init = () => {
@@ -113,6 +180,9 @@ init = () => {
 			oldCanplay = v.oncanplay;
 			v.oncanplay = onCanplay;
 			this.disconnect();
+			document.addEventListener('keydown', hotKey, !1);
+			if (playerInfo.timeCSS)
+				totalTime = getAllDuration(playerInfo.timeCSS);
 		}
 	}).observe(document.documentElement, {
 		childList : true,
@@ -120,8 +190,17 @@ init = () => {
 	});
 };
 
+if (mDomain !== 'panda') init();
+
 switch (mDomain) {
 case 'qq':
+	playerInfo = {
+		disableSpace: true,
+		//playCSS: 'txpdiv.txp_btn_play',
+		nextCSS: 'txpdiv.txp_btn_next',
+		webfullCSS: 'txpdiv.txp_btn_fake[data-status="false"]',
+		fullCSS: 'txpdiv.txp_btn_fullscreen'
+	};
 	fakeUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:48.0) Gecko/20100101 Firefox/48.0');
 	break;
 case 'bilibili':
@@ -129,41 +208,50 @@ case 'bilibili':
 	break;
 case 'youku':
 	sessionStorage.P_l_h5 = 1;
-	init();
+	playerInfo = {
+		disableSpace: true,
+		//playCSS: 'button.control-play-icon',
+		nextCSS: 'button.control-next-video',
+		fullCSS: 'button.control-fullscreen-icon'
+	};
 	break;
 case 'cctv':
 case 'cntv':
 	fakeUA(ua_samsung);
-	init();
 	break;
 case 'sina':
 	fakeUA('Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3');
-case 'weibo':
-	init();
+//case 'weibo':
 	break;
 case 'le':
 case 'lesports':
 	if (!window.mozInnerScreenX) {//firefox黑屏
 		fakeUA('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 Version/7.0.3 Safari/7046A194A');
-		siteFn = () => {
-			totalTime = __INFO__.video.duration;
-			//__INFO__.video.adseed = 0;
+		//totalTime = __INFO__.video.duration;
+		playerInfo = {
+			timeCSS: 'span.time_total',
+			nextCSS: 'div.hv_ico_next',
+			fullCSS: 'div.hv_ico_screen'
 		};
-		init();
 	}
 	break;
 case 'sohu':
 	fakeUA(ua_samsung);
-	// q('meta[name=mobile-agent]').remove();
-	siteFn = () => {
-		totalTime = getAllDuration('span.x-duration-txt');
+	playerInfo = {
+		timeCSS: 'span.x-duration-txt',
+		nextCSS: 'li.on[data-vid]+li > a',
+		fullCSS: 'div.x-fs-btn'
 	};
-	init();
 	break;
 case 'fun':
 	if (u.startsWith('m.')) {
 		/^\/[mv]play/.test(path) && location.assign(path.replace('/', '/i') + location.search);
-		path.includes('play') && init();
+		if (path.includes('play')) {
+			playerInfo = {
+				nextCSS: 'a.btn.next-btn',
+				fullCSS: 'a.btn.full-btn'
+			};
+		}
 		return;
 	}
 	let vid = path.r1(/\bv-(\d+)/);
@@ -181,9 +269,8 @@ case 'fun':
 	}, !1);
 	break;
 case 'tudou':
-	siteFn = () => {
+	playerInfo.onMetadata = () => {
 		//获取播放时长
-		//totalTime = getAllDuration('span.td-h5__player__console__time-total');
 		totalTime = ~~q('meta[name=duration]').getAttribute('content');
 		const cur = ~~v.duration +1;
 		//console.log(cur, totalTime);
@@ -198,7 +285,6 @@ case 'tudou':
 			}, 9);
 		}
 	};
-	init();
 	break;
 case 'panda':
 	localStorage.setItem('panda.tv/user/player', '{"useH5player": 1}');
